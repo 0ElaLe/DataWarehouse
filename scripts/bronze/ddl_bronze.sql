89d1580e75f2161c23271f98162b3012a9c4948c
0ElaLe <84888100+0ElaLe@users.noreply.github.com>
2025-10-02 00:44:23 -0600

    Update and rename DDL_BRONZE.sql to ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -20,27 +20,35 @@ CREATE SCHEMA silver;
 GO
 CREATE SCHEMA gold;
 GO
-
+--Creando Las tablas
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+DROP TABLE bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info(
 cst_id INT,
-cast_key  NVARCHAR(50),
+cst_key  NVARCHAR(50),
 cst_firstname NVARCHAR(50),
 cst_lastname NVARCHAR(50),
 cst_marital_status NVARCHAR(50),
 cst_gndr NVARCHAR(50),
 cst_create_date Date
 );
+GO
 
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+DROP TABLE bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info(
 prd_id INT,
 prd_key NVARCHAR(50),
 prd_nm NVARCHAR(50),
 prd_cost INT,
 prd_line NVARCHAR(50),
-prd_start_dt DATE,
-prd_end_dt DATE
+prd_start_dt DATETIME,
+prd_end_dt DATETIME
 );
+GO	
 
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+DROP TABLE bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details(
 sls_ord_num NVARCHAR(50),
 sls_prd_key NVARCHAR(50),
@@ -53,7 +61,10 @@ sls_quantity INT,
 sls_price INT
 );
 
-CREATE TABLE  bronze.erp_csut_az12(
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+DROP TABLE bronze.erp_cust_az12;
+CREATE TABLE  bronze.erp_cust_az12(
 cid NVARCHAR(50),
 bdate DATE,
 gen NVARCHAR(50)
@@ -67,11 +78,8 @@ maintenance NVARCHAR(50)
 );
 GO
 
-BULK INSERT bronze.crm_cust_info
-FROM '/var/opt/mssql/data/customers.csv'
-WITH(
-FIRSTROW =2, 
-FIELDTERMINATOR=',',
-TABLOCK
+CREATE TABLE bronze.erp_loc_a101 (
+cid NVARCHAR(50),
+cntry NVARCHAR(50)
 );
 GO
